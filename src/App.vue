be8f0d7373b7893730de9ab3ea1f6080df0722bb
Đoàn Minh Đức - CMC Global DU3 <dmduc4@ldap.cmcglobal.com.vn>
2021-10-29 14:29:35 +0700

    add css

---
+++ src/App.vue
@@ -0,0 +1,75 @@
+<template>
+  <div id="app">
+    <main>
+      <div class="search-box">
+        <input 
+          type="text" 
+          class="search-bar" 
+          placeholder="Search..."
+          v-model="query"
+          @keypress="fetchWeather"
+        />
+      </div>
+
+      <div class="weather-wrap">
+        <div class="location-box">
+          <div class="location"></div>
+          <div class="date"></div>
+        </div>
+
+        <div class="weather-box">
+          <div class="temp"></div>
+          <div class="weather"></div>
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      api_key: '88cf803e801ec42c63ad742a8bb2d768'
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  font-family: 'montserrat', sans-serif;
+}
+#app {
+  background-image: url('./assets/cold-bg.jpg');
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.4s;
+
+  main {
+    min-height: 100vh;
+    padding: 25px;
+    background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.75));
+
+    .search-box {
+      width: 100%;
+      margin-bottom: 30px;
+
+      .search-bar {
+        display: block;
+        width: 100%;
+        padding: 15px;
+        color: #313131;
+
+      }
+    }
+  }
+}
+</style>
